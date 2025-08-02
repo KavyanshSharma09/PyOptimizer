49ffa5699563d165adf6cc14db153b4e38f39d6b
KavyanshSharma09 <112067780+KavyanshSharma09@users.noreply.github.com>
2025-08-02 16:44:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='code-optimizer',
+    name='PyOptimizer',
     version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Kavyansh',
+    author_email='Kavyanshsharma2004@gmail.com',
     description='A project to detect computation-heavy programs and optimize them using GPU acceleration.',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
@@ -15,8 +15,7 @@ setup(
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+       
     ],
     python_requires='>=3.6',
 )
